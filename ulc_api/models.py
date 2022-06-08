99386dd146ebdd0c52019abc5bd1a8f25eda9bcf
etamar <etamar.levy@gmail.com>
2022-06-07 21:25:25 -0400

    created user migration

--- ulc_api/models.py
+++ ulc_api/models.py
@@ -18,6 +18,7 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+        
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
