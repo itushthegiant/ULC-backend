fffa65aa3d7afdf96dca4a6a1ad69b1e6f7afaf8
etamar <etamar.levy@gmail.com>
2022-06-13 00:05:56 -0400

    Deleted ProfileFeedItem model

--- ulc_api/serializers.py
+++ ulc_api/serializers.py
@@ -5,11 +5,6 @@ from . import models
 
 
 
-# class HelloSerializer(serializers.Serializer):
-#     """Serializes a name field testing our APIView"""
-#     name = serializers.CharField(max_length=10)
-
-
 class UserProfileSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
 
@@ -42,10 +37,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
-class ProfileFeedItemSerializer(serializers.ModelSerializer):
-    """Serializes profile feed items"""
+# class ProfileFeedItemSerializer(serializers.ModelSerializer):
+#     """Serializes profile feed items"""
 
-    class Meta:
-        model = models.ProfileFeedItem
-        fields = ('id', 'user_profile', 'status_text', 'created_on')
-        extra_kwargs = {'user_profile': {'read_only': True}}
+#     class Meta:
+#         model = models.ProfileFeedItem
+#         fields = ('id', 'user_profile', 'status_text', 'created_on')
+#         extra_kwargs = {'user_profile': {'read_only': True}}
